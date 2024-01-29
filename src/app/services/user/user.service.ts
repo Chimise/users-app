1eeb7969926dde97cf514ba949acb34bed5d2a0f
chisom promise <chimisepro@gmail.com>
2024-01-29 22:17:26 +0100

    feat: create user service

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,11 +1,24 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { HTTP_CONFIG, HttpConfig } from '../../config/httpclient.config';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() {
+  constructor(private httpClient: HttpClient, @Inject(HTTP_CONFIG) private httpConfig: HttpConfig) {
 
   }
+
+  getAllUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.httpConfig.withBase("/users"), { headers: this.httpConfig.defaultHeaders });
+  }
+
+  getUserById(id: string): Observable<User> {
+    return this.httpClient.get<User>(this.httpConfig.withBase(`/users/${id}`), { headers: this.httpConfig.defaultHeaders });
+  }
+
 }
