136b15213a299a37bc13ef5eb80dfc1e010d4393
chisom promise <chimisepro@gmail.com>
2024-01-31 22:17:47 +0100

    feat: update design of the userslist page

--- src/app/pages/userdetails/userdetails.component.ts
+++ src/app/pages/userdetails/userdetails.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user/user.service';
+import { Subscription } from 'rxjs';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-userdetails',
@@ -8,13 +11,21 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './userdetails.component.html',
   styleUrl: './userdetails.component.css'
 })
-export class UserdetailsComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+export class UserdetailsComponent implements OnInit, OnDestroy {
+  constructor(private route: ActivatedRoute, private userService: UserService) { }
 
   id!: string;
+  loading: boolean = true;
+  error: string | null = null;
+  subscription!: Subscription;
+  user!: User;
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params["id"];
-    console.log(this.id);
+    this.subscription = this.userService.getUserById(this.id).subscribe((user) => this.user = user, (error) => this.error = "An error occurred, please try again", () => this.loading = true);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
